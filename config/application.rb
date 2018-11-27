2b418d8f14178635a5d2c8ab9b2ae5c434ab255f
mickplayer <mickgamehome@gmail.com>
2018-11-28 02:46:39 +0800

    ImageDb.images_path

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,10 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module ImageDb
+  def self.images_path
+    Rails.root.join 'images'
+  end
+
   class Application < Rails::Application
     config.load_defaults 5.2
   end
