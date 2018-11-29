ad49d566460e7b21e7f88a675f00d50f5053bb5e
mickplayer <mickgamehome@gmail.com>
2018-11-29 23:23:45 +0800

    Add ClearImagesJob

---
+++ app/jobs/clear_images_job.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class ClearImagesJob < ApplicationJob
+  queue_as :default
+
+  def perform
+    image_ids = []
+    Image.find_each do |image|
+      file_path = ImageDb.images_path.join(image.file_name)
+      next if File.file?(file_path) && File.exist?(file_path)
+
+      image_ids.push(image.id)
+    end
+
+    return if image_ids.blank?
+
+    image_ids.each_slice(100) do |ids|
+      Image.where(id: ids).delete_all
+    end
+  end
+end
