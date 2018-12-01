cf1e9b62ec66d129462fcf2cb5aa724600cc0bfe
mickplayer <mickgamehome@gmail.com>
2018-12-02 00:09:44 +0800

    Fix query images job

--- app/jobs/query_images_job.rb
+++ app/jobs/query_images_job.rb
@@ -3,15 +3,16 @@
 class QueryImagesJob < ApplicationJob
   queue_as :default
 
+  rescue_from 'HTTParty::ResponseError' do |error|
+    logger.ap 'rescue_from \'HTTParty::ResponseError\''
+    if error.response.is_a?(Net::HTTPTooManyRequests) && executions < 15
+      retry_job wait: 1.minute
+    end
+  end
+
   def perform
     Image.not_queried.find_each(batch_size: 10) do |image|
       QueryImageJob.perform_now(image.file_name)
     end
-  rescue RestClient::TooManyRequests
-    @retry_count = @retry_count.to_i + 1
-    return if @retry_count > 15
-
-    sleep 60
-    retry
   end
 end
