83212553765adca297dc3858868b36d196a028a7
mickplayer <mickgamehome@gmail.com>
2018-12-01 20:36:48 +0800

    Fix rescue

--- app/jobs/query_images_job.rb
+++ app/jobs/query_images_job.rb
@@ -3,11 +3,15 @@
 class QueryImagesJob < ApplicationJob
   queue_as :default
 
-  rescue_from 'RestClient::TooManyRequests', wait: 60, attempts: 15
-
   def perform
     Image.not_queried.find_each(batch_size: 10) do |image|
       QueryImageJob.perform_now(image.file_name)
     end
+  rescue RestClient::TooManyRequests
+    @retry_count = @retry_count.to_i + 1
+    return if @retry_count > 15
+
+    sleep 60
+    retry
   end
 end
