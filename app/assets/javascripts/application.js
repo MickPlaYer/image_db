d0499b8c964c6702b894f58b94f8308e33eae16c
mickplayer <mickgamehome@gmail.com>
2020-01-31 02:45:48 +0800

    GO TO TOP

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,19 @@
 //= require activestorage
 //= require turbolinks
 //= require cable
+
+document.addEventListener('DOMContentLoaded', function() {
+  const goToTop = document.querySelector('.go-to-top');
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 600) {
+      goToTop.classList.add('show');
+    } else {
+      goToTop.classList.remove('show');
+    }
+  });
+  goToTop.addEventListener('click', function() {
+    window.scrollTo({ behavior: 'smooth', top: 0 });
+    goToTop.classList.remove('show');
+    goToTop.blur();
+  });
+});
